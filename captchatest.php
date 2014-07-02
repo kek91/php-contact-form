eba46726b26296b29be5a5886ce06503dbed12f0
KEK <norkek@gmail.com>
2014-07-02 15:25:40 +0200

    Fixed XSS injection vulnerability, working on captcha, etc

---
+++ captchatest.php
@@ -0,0 +1,83 @@
+<?php 
+///////////////////////////////////////////////////////////////////
+//                                                                /
+//    ASCII PHP Captcha                                           /
+//    Author: http://phpsnips.com/71/ASCII-CAPTCHA                /
+//    Modified by Teknix / Kim Eirik Kvassheim <kek@teknix.no>    /
+//                                                                /
+///////////////////////////////////////////////////////////////////
+
+
+$string = md5(rand(0,5000)); 
+$string = substr($string,0,5); 
+$letters = str_split($string); 
+
+$iw = 160;  // image width
+$ih = 50;   // image height
+$fs = 24;   // font size
+$image = imagecreatetruecolor($iw,$ih); 
+$white = imagecolorallocate($image, 255, 255, 255); 
+$black = imagecolorallocate($image, 0, 0, 0); 
+imagefilledrectangle($image, 0, 0, $iw, $ih, $white); 
+imagerectangle($image, 0, 0, 159, 49, $black);
+
+// Adding some static noise
+
+for($i=0; $i<500; $i++)
+{
+    @imagesetpixel($image, rand(1,160), rand(1,50), imagecolorallocate($image, rand(200,250), rand(200,250), rand(200,250)));   
+}
+
+for($i=0; $i<50; $i++)
+{
+    @imagesetpixel($image, rand(1,160), rand(1,50), imagecolorallocate($image, rand(50,100), rand(50,100), rand(50,100)));  
+}
+
+for($i=0; $i<50; $i++)
+{
+    imageline($image, rand(1,$iw), rand(1,$ih), rand(1,$iw), rand(1,$ih), imagecolorallocate($image, rand(200,250), rand(200,250), rand(200,250))); 
+}
+
+$y_min = ($ih / 2) + ($fs / 3) - 2; // Set minimum letter y position 
+$y_max = ($ih / 2) + ($fs / 3) + 2; // Set maximum letter y position 
+$x = $fs / 2; // Set the letter starting point 
+
+$i = 0; 
+foreach($letters as $letter){ 
+    // Set the angle of the letter random from -45 and 45 
+    $angle = rand(-30, 30); 
+    // Set a random y position of the letter using using the above min and max 
+    $y = rand($y_min, $y_max); 
+    // check to see if this is first letter, yes then skip next line otherwise, 
+    // add font size to x to move letter to the right 
+    if($i != 0) $x += $fs; 
+    $font_color = imagecolorallocate($image, rand(50,200), rand(50,200), rand(50,200)); 
+    imagettftext($image, $fs, $angle, $x, $y, $font_color, 'bebas.ttf', $letter); 
+    // Incrament $i 
+    $i++; 
+} 
+// Finally build the image! 
+$black = imagecolorallocate($image, 0, 0, 0); 
+// Build the ASCII image 
+echo '<div id="captcha" style="line-height:1px; font-size:1px;">'; 
+for($h=0;$h<$ih;$h++){ 
+    for($w=0;$w<=$iw;$w++){ 
+        $rgb = imagecolorat($image, $w, $h); 
+        $r = ($rgb >> 16) & 0xFF; 
+        $g = ($rgb >> 8) & 0xFF; 
+        $b = $rgb & 0xFF; 
+        if($w == $iw){ 
+            echo '<br />'; 
+        }else{ 
+            echo '<span style="color:rgb('.$r.','.$g.','.$b.');">#</span>'; 
+        } 
+    } 
+} 
+echo '</div>'; 
+//End ASCII Image Build 
+$_SESSION['CAPTCHA'] = $string; 
+?> 
+
+<p>
+    Image String: <?php echo $_SESSION['CAPTCHA']; ?> 
+</p>
--- contact.php
+++ contact.php
@@ -1,85 +1,150 @@
 <?php
 session_start(); 
-// Erase this session_start(); if including the script from another file
+// If you're including this file from another file, remember to use session_start(); in the beginning of your script.
 
-/*******************************************************
-* Simple contact form in PHP                           *
-* Author: Teknix / Kim Eirik Kvassheim <kek@teknix.no> *
-* Version: 0.0.1                                       *
-********************************************************/
+///////////////////////////////////////////////////////////////
+//                                                            /
+//    Simple contact form in PHP                              /
+//    Author: Teknix / Kim Eirik Kvassheim <kek@teknix.no>    /
+//    Version: 0.0.1                                          /
+//                                                            /
+///////////////////////////////////////////////////////////////
 
-// Edit the following variables
-$mail_to        = "";
-$mail_cc        = ""; // Optional
-$mail_bcc       = ""; // Optional
 
-// Validation
-//include_once("validate.class.php");
+// Config: Mail settings
+$mail_to            = "";
+$mail_cc            = ""; // Optional
+$mail_bcc           = ""; // Optional
+$phone_countrycode  = "+47";
+// Config: Error messages
+$missing_name = "Your name field is empty. Please try again.";
+$invalid_number = "The telephone number provided seems to be invalid. Please try again.";
+$invalid_email = "The email address provided seems to be invalid. Please try again.";
+$missing_message = "Message field is empty. Please try again.";
+$invalid_token = "Auth token was not provided. If this is an XSS injection attempt then you are mean. Shoo!";
+// End of config, no need to change anything below
+
+
+// Functions
 function escape($str)
 {
-    echo "Before escape: $str";
-    $str = trim($str);
-    $str = htmlentities($str, ENT_QUOTES, 'UTF-8');
-    echo "<br><br>After escape: $str";
-    //return $str;
+    $str = htmlentities(trim($str), ENT_QUOTES, 'UTF-8');
+    return $str;
+}
+function escapeNumber($num)
+{
+    $num = preg_replace('/[^0-9]/', '', $num);
+    return $num;
 }
 
+
 // Check if form has been submitted
-if(!empty($_POST))
+if(!empty($_POST['name']))
 {
-    if(isset($_SESSION['formToken'] && $_POST['formToken'] === $_SESSION['formToken']))
+    if(!empty($_SESSION['formToken']) && !empty($_POST['formToken']) && $_POST['formToken'] === $_SESSION['formToken'])
     {
         // Form token is ok! Now we check the input fields
-        $name = escape($_POST['name']);
-        $email
+        // The token is used to prevent XSS attacks
+        $name       = escape($_POST['name']);
+        $email      = escape($_POST['email']);
+        $telephone  = escapeNumber($_POST['telephone']);
+        $message    = escape($_POST['message']);
 
+        $error      = false;
+        $errorlist  = "";
 
-
-
-
-
-
-
-        $validate = new Validate();
-        $validation = $validate->check($_POST, array(
-            'name' => array(
-                'required' => true,
-                'min' => 3,
-                'max' => 30
-            ),
-            'email' => array(
-                'required' => true,
-                'mailcheck' => true
-            ),
-            'telephone' => array(
-                'required' => false,
-                'telephone' => true
-            )
-        ));
-
-        if($validation->passed())
+        if(empty($name))
         {
+            $error = true;
+            $errorlist .= $missing_name;
+        }
+        elseif(!ctype_digit($telephone))
+        {
+            $error = true;
+            $errorlist .= $invalid_number;
+        }
+        elseif(!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            $error = true;
+            $errorlist .= $invalid_email;
+        }
+        elseif(empty($message))
+        {
+            $error = true;
+            $errorlist .= $missing_message;
+        }
+        else
+        {
+            // Success
+        }
+
+
+
+        /*
+
+
+
+
             // All input fields are ok, let's check the captcha
             $captchaQ1 = intval($_POST['captchaQ1']);
             $captchaQ2 = intval($_POST['captchaQ2']);
             $captchaOp = $_POST['captchaOp'];
             $captchaAnswer = $captchaQ1.$captchaOp.$captchaQ2;
         }
+        */
     }
     else
     {
-        /* 
-        * No valid token were supplied 
-        * meaning someone tried to submit 
-        * without going through the form.
-        * This will return false 
-        */
-        return false;
+        /* No valid token were supplied 
+        meaning someone tried to submit 
+        without going through the HTML form. */
+        echo $invalid_token;
     }
 }
+
+
 
 // If form has not been submitted we show the HTML form
 else
 {
-
+    ?>
+    <style>
+        table, td, input, textarea     { padding:5px; font-family:Verdana,Helvetica,Arial,sans-serif; font-size:12px; }
+        input[type=submit]      { cursor:pointer; padding:10px 5px 10px 5px; }
+        textarea                { width:300px; height:100px; }
+    </style>
+    <form action="contact.php" method="post">
+        <table>
+            <tr>
+                <th colspan="3">Contact form</th>
+            </tr>
+            <tr>
+                <td><label for="name">Name</label></td>
+                <td></td>
+                <td><input type="text" name="name" id="name" required></td>
+            </tr>
+            <tr>
+                <td><label for="telephone">Telephone</label></td>
+                <td><?php echo $phone_countrycode; ?></td>
+                <td><input type="tel" name="telephone" id="telephone" required></td>
+            </tr>
+            <tr>
+                <td><label for="email">Email</label></td>
+                <td></td>
+                <td><input type="email" name="email" id="email" required></td>
+            </tr>
+            <tr>
+                <td><label for="message">Message</label></td>
+                <td></td>
+                <td><textarea name="message" id="message" required></textarea></td>
+            </tr>
+            <tr>
+                <td colspan="3">
+                    <input type="hidden" name="formToken" value="<?php echo $_SESSION['formToken'] = md5(mt_rand()); ?>">
+                    <input type="submit" value="Send message" style="width:100%;">
+                </td>
+            </tr>
+        </table>
+    </form>
+    <?php
 }
